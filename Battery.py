2158ad5a91412a8477f30358321cb9418da4ca39
coolman28129 <91056752+coolman28129@users.noreply.github.com>
2021-10-30 22:18:07 +0700

    Update Battery.py

--- Battery.py
+++ Battery.py
@@ -19,7 +19,7 @@ print("@#####################################################@")
 print("")
 
 # Current version
-print("Current version 1.2")
+print("Current version 1")
 
 # New line
 print("")
@@ -30,58 +30,64 @@ print("Battery left will be named as 'BatteryTime.POWER_TIME_UNLIMITED' if charg
 print("Battery left will be in seconds and updated after the battery percentage changes")
 
 # New line
-print("")
+dprint("")
 
 # Variable
 rmax = 74
 rmin = 41
+sleeplevel = 10
 battery = psutil.sensors_battery()
 Check = True
 toast = ToastNotifier()
+sleep = False
+def convertTime(seconds):
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    return "%d:%02d:%02d" % (hours, minutes, seconds)
 
-# Script Settings Output
+# Script settings
 print("Max battery percentage :", rmax)
 print("Min battery percentage :", rmin)
 
 # New line
 print("")
 
-# Always checking
+# Checks
 while Check == True:
     battery = psutil.sensors_battery()
-    def convertTime(seconds):
-        minutes, seconds = divmod(seconds, 60)
-        hours, minutes = divmod(minutes, 60)
-        return "%d:%02d:%02d" % (hours, minutes, seconds)
     print("Battery percentage :", battery.percent, "%")
     battery = psutil.sensors_battery()
     print("Power plugged in :", battery.power_plugged)
     battery = psutil.sensors_battery()
-    print("Battery left :", battery.secsleft)
-    battery = psutil.sensors_battery()
+    if battery.power_plugged == False:
+        print("Battery left :", convertTime(battery.secsleft))
+        battery = psutil.sensors_battery()
+    if battery.power_plugged == True:
+        print("Battery left :", battery.secsleft)
+        battery = psutil.sensors_battery()    
     if battery.percent == rmax:
         battery = psutil.sensors_battery()
         if battery.power_plugged == True:
             battery = psutil.sensors_battery()
             print("Please unplug your charger")
-            toast.show_toast("Battery v1.1", "Please unplug your charger.")
+            toast.show_toast("Battery v1", "Please unplug your charger.")
     if battery.percent > rmax:
         battery = psutil.sensors_battery()
         if battery.power_plugged == True:
             battery = psutil.sensors_battery()
             print("Please unplug your charger")
-            toast.show_toast("Battery v1.1", "Please unplug your charger.")
+            toast.show_toast("Battery v1", "Please unplug your charger.")
     if battery.percent < rmin:
         battery = psutil.sensors_battery()
         if battery.power_plugged == False:
             battery = psutil.sensors_battery()
             print("Please plug in your charger")
-            toast.show_toast("Battery v1.1", "Please charge.")
+            toast.show_toast("Battery v1", "Please charge.")
     if battery.percent == rmin:
         battery = psutil.sensors_battery()
         if battery.power_plugged == False:
             battery = psutil.sensors_battery()
             print("Please plug in your charger")
-            toast.show_toast("Battery v1.1", "Please charge.")
+            toast.show_toast("Battery v1", "Please charge.", duration = 1)
     print("")
     time.sleep(1)
